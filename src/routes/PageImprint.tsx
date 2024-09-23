3c257cc83f1c1dc2b30578c4dfa8e0dd10df68e7
Deniz Petzold <deniz.petzold@gmail.com>
2024-09-23 23:21:06 +0300

    fixed email

--- src/routes/PageImprint.tsx
+++ src/routes/PageImprint.tsx
@@ -18,7 +18,7 @@ export function PageImprint() {
             <h2 className="text-2xl md:text-3xl text-zinc-800 font-medium dark:text-zinc-200 text-left">Kontakt</h2>
             <p className="text-lg md:text-xl text-zinc-700 dark:text-zinc-400 text-left">
                 Telefon: 015678 402840<br />
-                E-Mail: info@egenus.app<br />
+                E-Mail: deniz.petzold@gmail.com<br />
             </p>
         </div>
     )
